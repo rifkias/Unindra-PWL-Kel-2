e4fe9ec6a597b283e02fa84b7228ac8cd84e0b25
Rifki Alfariz Shidiq <rifki.alfariz@lincgrp.com>
2024-07-29 22:53:53 +0700

    update stock

--- php/stock/stock.php
+++ php/stock/stock.php
@@ -4,6 +4,55 @@ include '../../helper.php';
 
 $uri = getUri();
 checkAuth();
+
+if (isset($_POST['update'])) {
+    $idx = $_POST['idbrg'];
+    $nama = $_POST['nama'];
+    $jenis = $_POST['jenis'];
+    $merk = $_POST['merk'];
+    $ukuran = $_POST['ukuran'];
+    $satuan = $_POST['satuan'];
+    $lokasi = $_POST['lokasi'];
+
+    $updatedata = mysqli_query($conn, "update sstock_brg set nama='$nama', jenis='$jenis', merk='$merk', ukuran='$ukuran', satuan='$satuan', lokasi='$lokasi' where idx='$idx'");
+
+    //cek apakah berhasil
+    if ($updatedata) {
+
+        echo " <div class='alert alert-success'>
+                <strong>Success!</strong> Redirecting you back in 1 seconds.
+              </div>
+            <meta http-equiv='refresh' content='1; url= stock.php'/>  ";
+    } else {
+        echo "<div class='alert alert-warning'>
+                <strong>Failed!</strong> Redirecting you back in 1 seconds.
+              </div>
+             <meta http-equiv='refresh' content='1; url= stock.php'/> ";
+    }
+};
+
+if (isset($_POST['hapus'])) {
+    $idx = $_POST['idbrg'];
+
+    $delete = mysqli_query($conn, "delete from sstock_brg where idx='$idx'");
+    //hapus juga semua data barang ini di tabel keluar-masuk
+    $deltabelkeluar = mysqli_query($conn, "delete from sbrg_keluar where id='$idx'");
+    $deltabelmasuk = mysqli_query($conn, "delete from sbrg_masuk where id='$idx'");
+
+    //cek apakah berhasil
+    if ($delete && $deltabelkeluar && $deltabelmasuk) {
+
+        echo " <div class='alert alert-success'>
+                <strong>Success!</strong> Redirecting you back in 1 seconds.
+              </div>
+            <meta http-equiv='refresh' content='1; url= stock.php'/>  ";
+    } else {
+        echo "<div class='alert alert-warning'>
+                <strong>Failed!</strong> Redirecting you back in 1 seconds.
+              </div>
+             <meta http-equiv='refresh' content='1; url= stock.php'/> ";
+    }
+};
 ?>
 
 <!doctype html>
